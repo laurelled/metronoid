54cb950bc685759a92369d2876390db8272b30d0
laurelled <bfn.lorenzo@gmail.com>
2023-08-31 16:54:55 +0200

    add metronome component

---
+++ src/app/metronome/metronome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metronome',
+  templateUrl: './metronome.component.html',
+  styleUrls: ['./metronome.component.css']
+})
+export class MetronomeComponent {
+
+}
